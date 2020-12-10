462b27545316723b39bddea141cc12d73e9e8b0d
Mike Hosseini <52359548+mikehosseini@users.noreply.github.com>
2020-12-10 14:19:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,244 +1,189 @@
-// Constant containing the main URL for pulling list of movies from the TMDB API
-const APIURL =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a7e57cb63bb601a072aac4703302f6dc";
-// Constant containing the URL to access images from the TMDB API
-const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-// Constant containing the URL for search function from the TMDB API
-const SEARCHAPI =
-  "https://api.themoviedb.org/3/search/movie?&api_key=a7e57cb63bb601a072aac4703302f6dc&query=";
 
-// Pulls the element inside the "search" ID
-const search = document.getElementById("search");
-
-// Form for search bar
-const form = document.getElementById("form");
-
-// Used for listmovies to put all movies in main
-const main = document.getElementById("main");
-
-// ░█████╗░██╗░░░░░██████╗░███████╗███╗░░██╗
-// ██╔══██╗██║░░░░░██╔══██╗██╔════╝████╗░██║
-// ███████║██║░░░░░██║░░██║█████╗░░██╔██╗██║
-// ██╔══██║██║░░░░░██║░░██║██╔══╝░░██║╚████║
-// ██║░░██║███████╗██████╔╝███████╗██║░╚███║
-// ╚═╝░░╚═╝╚══════╝╚═════╝░╚══════╝╚═╝░░╚══╝
-
-// Constant's containing URLS to pull movies from the TMDB API in a sorted order
-// Sorted by Newest
-const NEWESTAPI =
-  "https://api.themoviedb.org/3/discover/movie?api_key=a7e57cb63bb601a072aac4703302f6dc&language=en-US&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=1&release_date.lte=2020-12-10&vote_count.gte=1";
-//Sorted by the Top Rated
-const TOPRATEDAPI =
-  "https://api.themoviedb.org/3/discover/movie?api_key=a7e57cb63bb601a072aac4703302f6dc&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&release_date.lte=2020-12-10&vote_count.gte=1";
-// Sorted by the Action genre
-const ACTIONAPI =
-  "https://api.themoviedb.org/3/discover/movie?api_key=a7e57cb63bb601a072aac4703302f6dc&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_count.gte=1&with_genres=28";
-// Sorted by the Drama genre
-const DRAMAAPI =
-  "https://api.themoviedb.org/3/discover/movie?api_key=a7e57cb63bb601a072aac4703302f6dc&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_count.gte=1&with_genres=18";
-// Sorted by the Thriller genre
-const THRILLERAPI =
-  "https://api.themoviedb.org/3/discover/movie?api_key=a7e57cb63bb601a072aac4703302f6dc&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_count.gte=1&with_genres=53";
-// Sorted by the Comedy genre
-const COMEDYAPI =
-  "https://api.themoviedb.org/3/discover/movie?api_key=a7e57cb63bb601a072aac4703302f6dc&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_count.gte=1&with_genres=35";
-// Sorted by the Family genre
-const FAMILYAPI =
-  "https://api.themoviedb.org/3/discover/movie?api_key=a7e57cb63bb601a072aac4703302f6dc&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_count.gte=1&with_genres=10751";
-
-// ██╗░░██╗███████╗██╗░░░██╗██╗███╗░░██╗
-// ██║░██╔╝██╔════╝██║░░░██║██║████╗░██║
-// █████═╝░█████╗░░╚██╗░██╔╝██║██╔██╗██║
-// ██╔═██╗░██╔══╝░░░╚████╔╝░██║██║╚████║
-// ██║░╚██╗███████╗░░╚██╔╝░░██║██║░╚███║
-// ╚═╝░░╚═╝╚══════╝░░░╚═╝░░░╚═╝╚═╝░░╚══╝
-
-//This will call the async function, getMovies when passing the URL link w/ api key
-getMovies(APIURL);
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
-//async functions will return promise
-//This func will return will suspend UNTIL the "await" expression
-// returns a promise that is fulfilled or rejected
-// We use this function to call and take information from the Website with our API key
-async function getMovies(url) {
-  const response = await fetch(url);
-  // https://developer.mozilla.org/en-US/docs/Web/API/Response
-  const responseData = await response.json();
-
-  // console.log(responseData);   //keep for testing purposes
-
-  //responseData is an object with the .results being tied to metadata from the TMDB when we call information from the API
-  listMovies(responseData.results);
+/* ----------------- Jocelyn -------------------*/
+/* selecting all the elements: border-box means that the width and height properties include the content, padding, and border, but do not include the margin */
+* {
+    box-sizing: border-box;
 }
 
-// ███╗░░░███╗██╗██╗░░██╗███████╗
-// ████╗░████║██║██║░██╔╝██╔════╝
-// ██╔████╔██║██║█████═╝░█████╗░░
-// ██║╚██╔╝██║██║██╔═██╗░██╔══╝░░
-// ██║░╚═╝░██║██║██║░╚██╗███████╗
-// ╚═╝░░░░░╚═╝╚═╝╚═╝░░╚═╝╚══════╝
-
-function listMovies(movies) {
-  // clears the inside of main tag
-  main.innerHTML = "";
-
-  // calls a function once for each element in the movie object, in order:
-  movies.forEach(movie => {
-    const {
-      poster_path,
-      title,
-      vote_average,
-      release_date,
-      popularity,
-      original_language,
-      overview,
-      id,
-      genre_ids
-    } = movie;
-
-    const movieEl = document.createElement("div");
-    movieEl.classList.add("movie");
-
-    //replace everything in main tag with divs below
-
-    movieEl.innerHTML = ` 
-        
-      <a href="https://www.themoviedb.org/movie/${id}" target="_blank">
-   
-            <img
-                src="${IMGPATH + poster_path}"
-                alt="${title}"
-            />
-            <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getClassByRate(
-                  vote_average
-                )}">${vote_average}</span>
-            </div>
-            <div class="release-date">
-                  <h3>Release Date:</h3>
-                  ${release_date}
-            </div>
-            <div class="popularity">
-                  <h3>Popularity:</h3>
-                  ${popularity}
-            </div>
-            <div class="genres">
-                  <h3>Genre ID's:</h3>
-                  ${genre_ids}
-            </div>
-            </div>  
-            <div class="originlanguage">
-                  <h3>Original Language:</h3>
-                  ${original_language}
-            </div>                
-            <div class="summary">
-                <h3>SUMMARY:</h3>
-                ${overview}
-            </div>
-         
-           </a>
-        `;
-
-    main.appendChild(movieEl);
-  });
+/* background img will be inside the html for whole page  */
+html{
+   background: url("https://cdn.glitch.com/679f8215-53b7-4d85-a959-e4c9f117e744%2Fblocks2.png?v=1607417368478");
+}
+body {
+/*    background-color: #161A29;     */
+    margin: auto;
+    font-family: 'Architects Daughter', cursive;
 }
 
-// ░█████╗░██╗░░░░░██╗░█████╗░███████╗
-// ██╔══██╗██║░░░░░██║██╔══██╗██╔════╝
-// ███████║██║░░░░░██║██║░░╚═╝█████╗░░
-// ██╔══██║██║░░░░░██║██║░░██╗██╔══╝░░
-// ██║░░██║███████╗██║╚█████╔╝███████╗
-// ╚═╝░░╚═╝╚══════╝╚═╝░╚════╝░╚══════╝
-
-// When we pull information from the API, this function will be called
-// the value of "ratings" will be checked against IF statements
-//if the vote is the value 1-10, it will be assigned a color to and transfer into the listmovies function where it is being called
-
-function getClassByRate(vote) {
-  if (vote >= 8) {
-    return "green";
-  } else if (vote >= 5) {
-    return "orange";
-  } else {
-    return "red";
-  }
+header {
+    display: flex;
+    justify-content: flex-end;
+  
 }
 
-// ░█████╗░██╗░░░░░██████╗░███████╗███╗░░██╗
-// ██╔══██╗██║░░░░░██╔══██╗██╔════╝████╗░██║
-// ███████║██║░░░░░██║░░██║█████╗░░██╔██╗██║
-// ██╔══██║██║░░░░░██║░░██║██╔══╝░░██║╚████║
-// ██║░░██║███████╗██████╔╝███████╗██║░╚███║
-// ╚═╝░░╚═╝╚══════╝╚═════╝░╚══════╝╚═╝░░╚══╝
 
-// The following are the event listeners that allow user input to change
-// the order in which the movies appear on the website
+/* SIZE-RELATED CSS configurations to search bar */
+.search, .input {
 
-// This is the Event Listener thats allows the search bar to receive user input
-// and add it to the SEARCHAPIURL to find movies
-form.addEventListener("submit", enterkey => {
-  enterkey.preventDefault();
-  const searchTerm = search.value;
-  if (searchTerm) {
-    getMovies(SEARCHAPI + searchTerm);
-    search.value = "";
-  }
-});
+    width: 33%;
+    padding: 0px 0px;
+    margin-right:910px;
+    transform: translateX(-15%);
 
-// This Event Listener waits for a user to click on the Newest nav-item
-// to call the getMovies function using the NEWEST API URL instead of the default one
-var newestMovies = document.getElementById("newest");
-newestMovies.addEventListener("click", () => {
-  event.preventDefault();
-  getMovies(NEWESTAPI);
-});
+}
 
-// This Event Listener waits for a user to click on the Newest nav-item
-// to call the getMovies function using the TOPRATED API URL instead of the default one
-var topRatedMovies = document.getElementById("topRated");
-topRatedMovies.addEventListener("click", () => {
-  event.preventDefault();
-  getMovies(TOPRATEDAPI);
-});
+/* this represents any input/text areas displaying placeholder text.  */
+/* Refers to the text within the searchbar  */
+/* :placeholder-shown {
+    width: 100%;
+    font-size: 20px;
+    color: red;
+    padding: 0.5rem 1rem;
+}  */
 
-// This Event Listener waits for a user to click on the Newest nav-item
-// to call the getMovies function using the ACTION API URL instead of the default one
-var actionMovies = document.getElementById("action");
-actionMovies.addEventListener("click", () => {
-  event.preventDefault();
-  getMovies(ACTIONAPI);
-});
 
-// This Event Listener waits for a user to click on the Newest nav-item
-// to call the getMovies function using the FAMILY API URL instead of the default one
-var familyMovies = document.getElementById("family");
-familyMovies.addEventListener("click", () => {
-  event.preventDefault();
-  getMovies(FAMILYAPI);
-});
+main {
+    display: flex;
+  /* The flex-wrap property specifies whether the flexible items should wrap or not. also Specifies that the flexible items will wrap, if necessary, in reverse order*/
+    flex-wrap: wrap-reverse;
+    margin:50px;
+    margin-left:125px;
+}
 
-// This Event Listener waits for a user to click on the Newest nav-item
-// to call the getMovies function using the DRAMA API URL instead of the default one
-var dramaMovies = document.getElementById("drama");
-dramaMovies.addEventListener("click", () => {
-  event.preventDefault();
-  getMovies(DRAMAAPI);
-});
+/* affects the sizing of the boxes */
 
-// This Event Listener waits for a user to click on the Newest nav-item
-// to call the getMovies function using the THRILLER API URL instead of the default one
-var thrillerMovies = document.getElementById("thriller");
-thrillerMovies.addEventListener("click", () => {
-  event.preventDefault();
-  getMovies(THRILLERAPI);
-});
+.movie {
+    width: 290px;
+    background-color: #1C3160;
+    border-radius: 3px;
+    box-shadow: 0 4px 5px rgba(0, 0, 0, 20.2);
+    overflow: hidden;
+    position: relative;
+    margin: 1.0rem;
+/*  margin-left:1%; */
 
-// This Event Listener waits for a user to click on the Newest nav-item
-// to call the getMovies function using the COMEDY API URL instead of the default one
-var comedyMovies = document.getElementById("comedy");
-comedyMovies.addEventListener("click", () => {
-  event.preventDefault();
-  getMovies(COMEDYAPI);
-});
+}
+
+/* changes sizing of image within the movie boxes*/
+
+.movie img {
+    width: 100%;
+}
+
+/* everything inside the movie boxes */
+.movie-info, .release-date, .popularity, .originlanguage, .genres {
+    color: #eee;
+    display: flex;
+    font-weight: bold;
+    justify-content: space-between;
+    padding: 0.5rem 1rem 1rem;
+    letter-spacing: 0.5px;
+}
+
+.movie-info h3 {
+    margin: 0;
+}
+
+.movie-info span {
+    background-color: #22254b;
+    border-radius: 20%;
+    font-weight: bold;
+    padding: 2px 8px;
+}
+
+/* setting color of the text for ratings 9-10 */
+.movie-info span.green {
+    color: rgb(39, 189, 39);
+}
+
+/* ratings 5-8 */
+.movie-info span.orange {
+    color: orange;
+}
+
+/* below 5 rating */
+.movie-info span.red {
+    color: rgb(189, 42, 42);
+}
+
+/* ----------------- Jocelyn -------------------*/
+
+
+
+/* ----------------- Kevin -------------------*/
+.summary {
+    background-color: #fff;
+    color: black;
+    font-weight: lighter;
+    padding: 2rem;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    max-height: 100%;
+    overflow: auto;
+    transform: translateX(-101%);
+    transition: transform 0.5s ease-in;
+}
+
+.summary h3 {
+    margin-top: 0;
+    font-weight: bold;
+}
+
+.movie:hover .summary {
+    transform: translate(0);
+
+}
+
+
+/* kc's parts */
+.movie:hover{
+  position:relative;
+  z-index: 1;
+  transition: all 0.32s ease-in-out;
+/*   transform: scale(1.24, 1.175); */
+  transform: translateY(-6%);
+/*   -webkit-transition: all .4s ease; */
+  border: 1px solid black;
+  box-shadow: 20px 20px 20px 10px rgba(6, 8, 10, 0.4);
+/*   box-shadow: 20px 20px 20px 2000px rgba(6, 8, 10, 0.4); */
+/* pseudo modal [not actually wanted compared to actual modal - remove transition delay to make this work] */
+ 
+}
+
+/* img-bg and alpha-bg originally used to define background, however this has now been moved to HTML tag */
+
+/* .img-bg {
+ overflow:hidden;
+ background: url("https://cdn.glitch.com/679f8215-53b7-4d85-a959-e4c9f117e744%2F54fcbfba-0e4a-43d1-8871-17257188be2b.image.png?v=1607633657165"); */
+/*     background-repeat: no-repeat; */
+/*     background-size: 100% 100%;   */
+/*   background-size: 50% 50%; */
+
+/* .alpha-bg {
+  overflow: hidden;
+  height: 100%;
+  background: rgba(15, 10, 30, 0.63);
+/*   background-repeat: repeat; */ */
+/*   background-size: cover; */
+/* }
+ */
+
+
+/* ----------------- alden -------------------*/
+
+/* required to make logo interactive with statement below */
+.center {
+  position:absolute;
+  width: 50%;
+  padding-top: 10px;
+}
+/* Gives the navbar items an animation while the mouse is hovering over them */
+.navbar-item:hover{
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+  transition: all 0.32s ease-in-out;
+  transform: translateY(-8%);
+
+}
